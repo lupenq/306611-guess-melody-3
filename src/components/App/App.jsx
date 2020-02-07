cf57521fc1c12f1e5503e28adfd0b5631232232f
Ilya Lupen <skinilya@gmail.com>
2020-02-07 14:27:02 +0300

    Добавил типизацию prop-types

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,23 @@
-import WelcomeScreen from '../WelcomeScreen/WelcomeScreen.jsx';
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
+import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
+
+const welcomeButtonHandler = () => {};
 
 const App = (props) => {
+  const {errorsCount} = props;
+
   return (
-    // eslint-disable-next-line react/prop-types
-    <WelcomeScreen errorsCount={props.errorsCount} />
+    <WelcomeScreen
+      errorsCount={errorsCount}
+      onWelcomeButtonClick={welcomeButtonHandler}
+    />
   );
 };
+
+App.propTypes = {
+  errorsCount: PropTypes.number.isRequired,
+};
+
 
 export default App;
