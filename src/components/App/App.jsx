7d83b405b06af639bef881153eeea742ddf1228b
Ilya Lupen <skinilya@gmail.com>
2020-02-10 11:28:07 +0300

    Добавил тесты

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
+import WelcomeScreen from "../WelcomeScreen/WelcomeScreen.jsx";
 
 const welcomeButtonHandler = () => {};
 
