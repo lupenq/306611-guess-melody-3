1bc2d2d3ef33a74668c0e367741869c3da4c3406
Ilya Lupen <skinilya@gmail.com>
2020-02-17 13:03:28 +0300

    Учебный проект: обратная сторона реальности №2

---
+++ src/components/question-artist/question-artist.test.js
@@ -0,0 +1,31 @@
+import QuestionArtist from './question-artist';
+
+
+const question = {
+  type: `artist`,
+  song: {
+    artist: `Jim Beam`,
+    src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+  },
+  answers: [{
+    picture: `https://api.adorable.io/avatars/128/0`,
+    artist: `John Snow`,
+  }, {
+    picture: `https://api.adorable.io/avatars/128/1`,
+    artist: `Jack Daniels`,
+  }, {
+    picture: `https://api.adorable.io/avatars/128/2`,
+    artist: `Jim Beam`,
+  }],
+};
+
+it(`QuestionArtist is rendered correctly`, () => {
+  const tree = renderer.create(
+      <QuestionArtist
+        question={question}
+        onAnswer={() => {}}
+      />
+  ).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
